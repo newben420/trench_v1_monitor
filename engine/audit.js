efeb1d975807da3bb8cac6e914fbe20679b990b8
newben420 <wraith@wraith.wraith>
2025-02-13 14:23:10 +0100

    working on pre-audit observer

--- engine/audit.js
+++ engine/audit.js
@@ -1,4 +1,37 @@
 class Audit {
+    /**
+     * @type {any[]}
+     */
+    static #queue = [];
+
+    /**
+     * @type {boolean}
+     */
+    static #isRunning = false;
+
+    /**
+     * Adds data to audit queue
+     * @param {any} data 
+     */
+    static addToQueue = (data) => {
+        Audit.#queue.push(data);
+        Audit.#run();
+    }
+
+    /**
+     * Runs audit on queue
+     */
+    static #run = () => {
+        if (Audit.#isRunning) {
+            return;
+        }
+        Audit.#isRunning = true;
+        while (Audit.#queue.length > 0) {
+            const data = Audit.#queue.shift();
+            // TODO
+        }
+        Audit.#isRunning = false;
+    }
 
 }
 
