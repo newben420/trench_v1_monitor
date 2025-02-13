9e0280fbace9faf5e24167ca6ba7582148534fb1
newben420 <wraith@wraith.wraith>
2025-02-13 15:23:01 +0100

    implemented pre audit observer - works - finishing up

--- engine/pre_audit_observer.js
+++ engine/pre_audit_observer.js
@@ -1,6 +1,7 @@
 const WebSocket = require('ws');
 const Site = require("./../env");
 const Log = require('../lib/log');
+const SolPrice = require('../lib/sol_price');
 
 class PreAuditObserver {
     /**
@@ -27,8 +28,8 @@ class PreAuditObserver {
             PreAuditObserver.#ws.send(JSON.stringify(payload));
             clearTimeout(PreAuditObserver.#memory[id].TOO);
             clearInterval(PreAuditObserver.#memory[id].SDO);
+            Log.flow(`PAO > Stopped observing '${PreAuditObserver.#memory[id].launchData.name}' at MC $${PreAuditObserver.#memory[id].observeData.mc}.`, 3);
             delete PreAuditObserver.#memory[id];
-            Log.flow(`PAO > Stopped observing ${id}.`, 3);
         }
 
     }
@@ -39,7 +40,7 @@ class PreAuditObserver {
             keys: [id]
         }
         PreAuditObserver.#ws.send(JSON.stringify(payload));
-        Log.flow(`PAO > Now observing ${id}.`, 3);
+        Log.flow(`PAO > Now observing '${PreAuditObserver.#memory[id].launchData.name}' at MC $${PreAuditObserver.#memory[id].observeData.mc}.`, 3);
     }
 
     static newToken = (data) => {
@@ -62,19 +63,24 @@ class PreAuditObserver {
                 buys: 0,
                 sells: 0,
                 holders: {},
-                mc: data.marketCapSol ?? 0,
+                mc: (data.marketCapSol ?? 0) * SolPrice.get(),
+                mcSol: data.marketCapSol ?? 0,
                 startTime: Date.now(),
+                price: 0,
                 lastUpdated: Date.now(),
             };
-            PreAuditObserver.#memory[id].observeData.holders[id] = data.initialBuy;
+            PreAuditObserver.#memory[id].observeData.holders[data.traderPublicKey.toString()] = data.initialBuy;
             PreAuditObserver.#startObservation(id);
+            // console.log(PreAuditObserver.#memory[id].observeData);
+            // console.log(data);
+            // console.log({ ...PreAuditObserver.#memory[id].observeData, holders: undefined });
         }
     }
 
     static newTrade = (data) => {
         if (data.txType === "buy" || data.txType === "sell") {
-            const id = data.mint;
-            const actor = data.traderPublicKey;
+            const id = data.mint.toString();
+            const actor = data.traderPublicKey.toString();
             if (PreAuditObserver.#memory[id]) {
                 PreAuditObserver.#memory[id].observeData.lastUpdated = Date.now();
                 if (data.txType === "buy") {
@@ -86,20 +92,22 @@ class PreAuditObserver {
                     // sell trade
                     PreAuditObserver.#memory[id].observeData.sells++;
                     PreAuditObserver.#memory[id].observeData.holders[actor] -= data.tokenAmount;
-                    // TODO - update to represent actual remaining sol amt below...and above, use actual from data
                     PreAuditObserver.#memory[id].observeData.holders[actor] = data.newTokenBalance;
                     if (data.newTokenBalance <= 0) {
                         delete PreAuditObserver.#memory[id].observeData.holders[actor];
                     }
                 }
-                const mcSol = data.marketCapSol;
-                // TODO - keep track of market cap
+                const mcSol = data.marketCapSol ?? 0;
+                PreAuditObserver.#memory[id].observeData.mc = mcSol * SolPrice.get();
+                PreAuditObserver.#memory[id].observeData.mcSol = mcSol;
+                PreAuditObserver.#memory[id].observeData.price = data.solAmount / data.tokenAmount;
+                // console.log({ ...PreAuditObserver.#memory[id].observeData, holders: [] });
                 // TODO - update market cap, convert to USD and check if audit should be triggered or not
                 // TODO -  before audit is triggered, calculate total supply so as to determine percentages of each holder to use in audit
             }
         }
         // TODO - remove below console.log when no longer needed
-        console.log(data);
+        // console.log(data);
     }
 }
 
